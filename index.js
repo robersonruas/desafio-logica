0f2685b1eae04e023f70b5cf99eeed48bbeed480
robersonruas <robersonruas@hotmail.com>
2024-03-16 18:40:15 -0300

    Atualização 1

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("Olá Mundo");
+nomeHeroi = "JinWoo";
+quantExperiencia = 8500
+
+if (quantExperiencia <=1000){
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Ferro")
+} else if (quantExperiencia >=1001 && quantExperiencia <=2000){
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Bronze " )
+}  else if (quantExperiencia >=2001 && quantExperiencia <=5000){
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Prata " )
+}   else if (quantExperiencia >=5001 && quantExperiencia <=7000){
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Ouro " )
+}   else if (quantExperiencia >=7001 && quantExperiencia <=8000){
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Platina " )
+}   else if (quantExperiencia >=8001 && quantExperiencia <=9000){
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Ascendente " )
+}   else if (quantExperiencia >=9001 && quantExperiencia <=10000){
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Imortal " )
+}  
+else {
+    console.log("O Herói de nome " +nomeHeroi +" está no nível de Radiante " )
+}
